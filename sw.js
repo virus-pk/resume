ee744d0816a0211e2add24a6da0a17e066272a13
Tauqeer Ahmad <tauqeerahmad89@gmail.com>
2019-03-21 02:19:48 +0500

    Re-build with base Route fixed in Nuxt config file

--- sw.js
+++ sw.js
@@ -1,24 +1,24 @@
-importScripts('/virus-pk.github.io/_nuxt/workbox.4c4f5ca6.js')
+importScripts('/resume/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/virus-pk.github.io/_nuxt/2a95d8f150789b64b2ba.js",
+    "url": "/resume/_nuxt/2a95d8f150789b64b2ba.js",
     "revision": "a0267a7e02d4dd431f3af24106838fe5"
   },
   {
-    "url": "/virus-pk.github.io/_nuxt/3bcf74752b6b2e790511.js",
-    "revision": "2b3689df2daa2f847eaf33ff4708281a"
-  },
-  {
-    "url": "/virus-pk.github.io/_nuxt/49d1a14327ed56516fc7.js",
-    "revision": "2400ceb6c0d2d2f441dcd699f28e9b3c"
-  },
-  {
-    "url": "/virus-pk.github.io/_nuxt/650f60fba46973b9b9a0.js",
+    "url": "/resume/_nuxt/650f60fba46973b9b9a0.js",
     "revision": "42b8195037dba3b5b8b733e1227e3336"
   },
   {
-    "url": "/virus-pk.github.io/_nuxt/fafccef5e38dbe276646.js",
+    "url": "/resume/_nuxt/9d979365c9159a537ee1.js",
+    "revision": "396aeeddcf8d789a7ae87386667a2561"
+  },
+  {
+    "url": "/resume/_nuxt/af5be39ea628ea0b00ec.js",
+    "revision": "c699cd04184f84cce1c89219c58bea8f"
+  },
+  {
+    "url": "/resume/_nuxt/fafccef5e38dbe276646.js",
     "revision": "7b27520230d991f4ff964c17a1882382"
   }
 ], {
@@ -30,6 +30,6 @@ workbox.precaching.precacheAndRoute([
 workbox.clientsClaim()
 workbox.skipWaiting()
 
-workbox.routing.registerRoute(new RegExp('/virus-pk.github.io/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/resume/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
 
-workbox.routing.registerRoute(new RegExp('/virus-pk.github.io/.*'), workbox.strategies.networkFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/resume/.*'), workbox.strategies.networkFirst({}), 'GET')
